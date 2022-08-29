8a95529e26ba5df9ce18ff479abcb955fd251fec
Efim Manevich <efimmanevich@gmail.com>
2022-08-29 09:13:19 +0300

    dump

--- SQL/block_list_add.sql
+++ SQL/block_list_add.sql
@@ -1,19 +1,26 @@
+--drop table blacklist;
+--update blacklist set priority = 0
 /*
 create table blacklist
 (
    card_number int8 primary key,
    block_date_time TIMESTAMP,
    unblock_date_time TIMESTAMP,
-   reason int4
+   reason int4,
+   priority int4 not null default(0)
 );
 */
 
+
+--drop index indx_date
 create index indx_date on blacklist (block_date_time);
 
+--drop view v_blacklist;
 create or replace view v_blacklist
 AS
-select card_number::text card_number, block_date_time, unblock_date_time, reason, card_number c_number from blacklist;
+select card_number::text card_number, block_date_time, unblock_date_time, reason, card_number c_number, "priority" from blacklist;
 
+--drop function p_blacklist_edit(_card_number int8,_block_date_time timestamp,_unblock_date_time timestamp,_reason int4)
 
 create function p_blacklist_edit
 
@@ -27,7 +34,7 @@ _card_number int8
 
 ,_reason int4 DEFAULT null
 
-
+,_priority int4 default 0
 
  )
 
@@ -50,6 +57,8 @@ _card_number int8
         ,unblock_date_time = _unblock_date_time
 
         ,reason = _reason
+
+        ,priority = COALESCE(_priority, 0)
 
             
 
@@ -75,7 +84,7 @@ _card_number int8
 
         ,reason 
 
-            
+        ,priority    
 
 	) 
 
@@ -88,6 +97,8 @@ _card_number int8
         ,_unblock_date_time
 
         ,_reason
+
+        ,COALESCE(_priority, 0)
 
              
 
@@ -105,7 +116,7 @@ $$ language plpgsql;
 
 
 
-create function p_blacklist_del (_card_number int8)
+create or replace function p_blacklist_del (_card_number int8)
 
 returns void
 
